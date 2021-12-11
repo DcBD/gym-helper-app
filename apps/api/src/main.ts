810e10bab2ce1f23af090114847b7cadd4e8bed7
David Prosba <david.prosba@gmail.com>
2021-12-11 21:53:32 +0100

    Init express and nest.js app

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -11,6 +11,10 @@ app.get('/api', (req, res) => {
   res.send({ message: 'Welcome to api!' });
 });
 
+app.get('/test', (req, res) => {
+  res.send({data: "OK"});
+})
+
 const port = process.env.port || 3333;
 const server = app.listen(port, () => {
   console.log(`Listening at http://localhost:${port}/api`);
